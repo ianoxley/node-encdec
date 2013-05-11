e7d17684d3f2dd09b045df3fce95f92eaa301692
Ian Oxley <ijoxley@gmail.com>
2013-05-11 21:08:54 +0100

    Added testing, linting, and file watching tasks to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    nodeunit: {
+      all: ['test/**/*.js']
+    },
+    watch: {
+      scripts: {
+        files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*/js'],
+        tasks: ['test'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['test']);
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
+};
