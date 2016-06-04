a14653292dea107c92353ea65f409848a5b6d3af
Ian Oxley <ijoxley@gmail.com>
2016-06-04 20:59:11 +0100

    Add base58 function.
    Expose base58 function, so callers can now call encdec.base58() to get
    a base58 encoder / decoder.

--- test/encdec.spec.js
+++ test/encdec.spec.js
@@ -7,41 +7,45 @@ describe('encdec', function() {
     var encdec;
     var base58;
 
-    beforeEach(function() {
-        encdec = require('../lib/encdec');
-        base58 = encdec.create();
-    });
+    describe('base58', function() {
+        beforeEach(function() {
+            encdec = require('../lib/encdec');
+            base58 = encdec.base58();
+        });
 
-    describe('encode', function() {
-        var tests = [
-            { args: [0], expected: '1' },
-            { args: [-1], expected: '' },
-            { args: [10002343], expected: 'Tgmc' },
-            { args: [1000], expected: 'if' }
-        ];
+        describe('encode', function() {
+            var tests = [
+                { args: [0], expected: '1' },
+                { args: [-1], expected: '' },
+                { args: [10002343], expected: 'Tgmc' },
+                { args: [1000], expected: 'if' }
+            ];
 
-        tests.forEach(function(test) {
-            it('base58 encodes ' + test.args[0] + ' as ' + test.expected, function() {
-                var result = base58.encode.apply(null, test.args);
-                assert.equal(result, test.expected);
+            tests.forEach(function(test) {
+                it('base58 encodes ' + test.args[0] + ' as ' + test.expected, function() {
+                    var result = base58.encode.apply(null, test.args);
+                    assert.equal(result, test.expected);
+                });
             });
         });
-    });
 
-    describe('decode', function() {
-        var tests = [
-            { args: ['Tgmc'], expected: 10002343 },
-            { args: ['if'], expected: 1000 },
-            { args: ['1'], expected: 0 },
-            { args: [''], expected: -1 },
-            { args: [{}], expected: -1 }
-        ];
+        describe('decode', function() {
+            var tests = [
+                { args: ['Tgmc'], expected: 10002343 },
+                { args: ['if'], expected: 1000 },
+                { args: ['1'], expected: 0 },
+                { args: [''], expected: -1 },
+                { args: [{}], expected: -1 }
+            ];
 
-        tests.forEach(function(test) {
-            it('base58 decodes ' + test.args[0] + ' as ' + test.expected, function() {
-                var result = base58.decode.apply(null, test.args);
-                assert.equal(result, test.expected);
+            tests.forEach(function(test) {
+                it('base58 decodes ' + test.args[0] + ' as ' + test.expected, function() {
+                    var result = base58.decode.apply(null, test.args);
+                    assert.equal(result, test.expected);
+                });
             });
         });
+
     });
+
 });
